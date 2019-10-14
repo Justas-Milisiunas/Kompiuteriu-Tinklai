2cb6cc2242c78e66091ef80002b02e7a3f757dc5
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-10-14 14:43:21 +0300

    Finished LD-2 1 part

---
+++ LD-2/1-dalis/data.php
@@ -0,0 +1,38 @@
+<?php
+$db_connection = mysqli_connect("localhost", "root", "", "kompiuteriu-tinklai");
+if (!$db_connection)
+    die("Mysql database unreachable: " . mysqli_error($db_connection));
+
+if ($_POST) {
+    insert_message();
+}
+
+function get_messages()
+{
+    global $db_connection;
+
+    $sql = "SELECT * FROM messages";
+    $result = mysqli_query($db_connection, $sql);
+    return mysqli_fetch_all($result);
+}
+
+function insert_message()
+{
+    global $db_connection;
+
+    $ip = get_client_ip();
+    $sql = "INSERT INTO messages (`name`, `email`, `message`, `receiver-email`, `ip`) 
+            VALUES ('{$_POST['name']}', '{$_POST['email']}', '{$_POST['message']}', '{$_POST['receiver-email']}', '{$ip}')";
+    var_dump($sql);
+    $result = mysqli_query($db_connection, $sql);
+
+    if ($result == true)
+        header("Location: index.php");
+    else
+        var_dump($result);
+}
+
+function get_client_ip()
+{
+    return $_SERVER['REMOTE_ADDR'] === '::1' ? '127.0.0.1' : $_SERVER['REMOTE_ADDR'];
+}
---
+++ LD-2/1-dalis/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>LD-2 1-dalis</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body class="test">
+<div id="page-content">
+    <div id="table-div">
+        <h2 class="headline">Žinutės</h2>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <td scope="col">#</td>
+                <td scope="col">Name</td>
+                <td scope="col">Email</td>
+                <td scope="col">Message</td>
+                <td scope="col">Receiver-Email</td>
+                <td scope="col">Ip</td>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            include "data.php";
+
+            $message = get_messages();
+            foreach ($message as $msg) {
+                echo "<tr>
+                <td>{$msg[0]}</td>
+                <td>{$msg[1]}</td>
+                <td>{$msg[2]}</td>
+                <td>{$msg[3]}</td>
+                <td>{$msg[4]}</td>
+                <td>{$msg[5]}</td>
+              </tr>";
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
+    <div id="form-div">
+        <div class="form-group">
+            <form method="post" action="data.php">
+                <label for="name">Siuntėjo vardas</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Jūsų vardas">
+
+                <label for="email">Siuntėjo e.paštas</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Jūsų e.paštas">
+
+                <label for="receiver-email">Gavėjo e.paštas</label>
+                <input type="email" class="form-control" id="receiver-email" name="receiver-email"
+                       placeholder="Gavėjo e.paštas">
+
+                <label for="message">Žinutė</label>
+                <textarea name="message" id="message" class="form-control" placeholder="Žinutė"></textarea>
+
+                <button type="submit" class="btn btn-primary btn-lg"> Siųsti</button>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
